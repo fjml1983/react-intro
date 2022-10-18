fe37e07813b895e882d847157765f3bc88bf77af
fjml1983 <fj.martinez@itsur.edu.mx>
2022-10-18 15:21:04 -0500

    1 file modified

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './style.css';
 import Car from './car/Car.js';
 
 export default function App() {
+  //Esta es una variable que representaría al arreglo de objetos JSON que se podrían recuperar desde una API Rest.
   const personajesDesdeAPI = [
     {
       nombre: 'Rayo McQueen',
@@ -23,12 +24,15 @@ export default function App() {
     },
   ];
 
+  //variable de estado que nos permitirá almacenar a los personajes a pintar (renderizar).
   const [personajes, setPersonajes] = useState([]);
 
+  //Un useEffect con dependencia de arreglo vacío [], siempre se dispara una vez al iniciar el componente.
   useEffect(() => {
-    //Realizar petición a la API utilizando FETCH
-    //asignar respuesta de la API a la variable personajesDesdeAPI
+    //TODO: Aquí iria la petición a la API utilizando FETCH y se asignaría la respuesta de la API a la variable personajesDesdeAPI, algo como esto:
     // personajesDesdeAPI = fetch("http://personajesdecars.com/all")
+    
+    //Se actualiza la variable de estado 'personajes' con los datos recuperados.
     setPersonajes(personajesDesdeAPI);
   }, []);
 
@@ -41,19 +45,19 @@ export default function App() {
         alignItems: 'center',
       }}
     >
-    {
-      personajes.map( personaje => 
-        <Car
-        nombre={personaje.nombre}
-        modelo={personaje.modelo}
-        imagen={personaje.imagen}
-        descripcion={personaje.descripcion}
-        sonido={personaje.sonido}
-        anio={personaje.anio}/>      
-      )
-    }
-
-      
+      {
+        //dibujar por cada personaje en la lista de personajes un componente Car.
+        personajes.map((personaje) => (
+          <Car
+            nombre={personaje.nombre}
+            modelo={personaje.modelo}
+            imagen={personaje.imagen}
+            descripcion={personaje.descripcion}
+            sonido={personaje.sonido}
+            anio={personaje.anio}
+          />
+        ))
+      }
     </div>
   );
 }
